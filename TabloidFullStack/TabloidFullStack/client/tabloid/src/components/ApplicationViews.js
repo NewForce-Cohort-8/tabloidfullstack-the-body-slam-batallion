0772622a4c65af48ef69c1e4104bd36bbc8f4c74
Darrin Daugherty <darrin@darrin.pro>
2024-02-05 17:29:14 -0500

    Basic GetAll function is working under Category Management tab

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
+import { CategoryList } from "./CategoryList";
+
 
 export default function ApplicationViews() {
 
  return(
       <Routes>
         <Route path="/" element={<Hello />} />
+        <Route path="/categories" element={<CategoryList/>} />
       </Routes>
    );
  
