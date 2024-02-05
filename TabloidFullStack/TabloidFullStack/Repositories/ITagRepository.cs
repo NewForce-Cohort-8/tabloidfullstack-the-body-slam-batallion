a52a91e03672cbcda9097e07774fe61f206b053a
Andrew Hanks <andymchanks@gmail.com>
2024-02-05 13:53:14 -0500

    API working alphabetically

---
+++ TabloidFullStack/TabloidFullStack/Repositories/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using TabloidFullStack.Models;
+
+namespace TabloidFullStack.Repositories
+{
+    public interface ITagRepository
+    {
+        //void AddTag(Tag tag);
+        //void DeleteTag(int tagId);
+        List<Tag> GetAll();
+        //Tag GetTagById(int id);
+        //void UpdateTag(Tag tag);
+    }
+}
---
+++ TabloidFullStack/TabloidFullStack/Repositories/TagRepository.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Data.SqlClient;
+using TabloidFullStack.Models;
+
+namespace TabloidFullStack.Repositories
+{
+    public class TagRepository :BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        //GetAll() Lists all tags
+        public List<Tag> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, Name FROM Tag
+                                        ORDER BY Name ASC;";
+                    List<Tag> tags = new List<Tag>();
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Tag tag = new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+                        tags.Add(tag);
+                    }
+
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+
+
+        //AddTag() Creates new tag
+        public void AddTag(Tag tag)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Tag ([Name])
+                    OUTPUT INSERTED.ID
+                    VALUES (@name)
+                    ";
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    tag.Id = id;
+                }
+            }
+        }
+
+        //Allows user to call up specific tag by Id
+        public Tag GetTagById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, [Name] FROM Tag
+                    WHERE Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        Tag tag = new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+
+                        reader.Close();
+                        return tag;
+                    }
+
+                    reader.Close();
+                    return null;
+                }
+            }
+        }
+        //Allows user to delete tag
+        public void DeleteTag(int tagId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    DELETE FROM Tag
+                    WHERE Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", tagId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //Allows user to edit tag
+        public void UpdateTag(Tag tag)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Tag
+                        SET [Name] = @name
+                        WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+                    cmd.Parameters.AddWithValue("@id", tag.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
