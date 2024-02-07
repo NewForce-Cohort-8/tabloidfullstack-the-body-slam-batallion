891f955134ac7999b07f531e4e5d09ef3b4d909f
JuanAlcalde <jjalcalde001@icloud.com>
2024-02-07 15:04:03 -0500

    fixed bug with myposts

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/PostManager.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/PostManager.js
@@ -6,20 +6,27 @@ export const getAllPosts = () => {
   return fetch(baseUrl) 
     .then((res) => res.json())
 };
+
+
 export const getPost = (id) => {
-  return fetch(`/api/post/${id}`).then((res) => res.json());
+  return fetch(`https://localhost:5001/api/post/${id}`).then((res) => res.json());
 };
-export const getAllPostsByUserProfile = (id) => {
-  return fetch(`https://localhost:5001/api/post/${id}`)
-    .then((res) => {
-      console.log("Response:", res); // Log the response object
-      return res.json(); // Parse the JSON response
-    })
+
+
+export const getAllPostsByUserProfile = (userId) => {
+  const url = `https://localhost:5001/api/post/userProfile/${userId}`;
+  return fetch(url)
+    .then((res) => res.json())
     .then((data) => {
-      console.log("Data:", data); // Log the parsed data
-      return data; // Return the parsed data
+      console.log("Data:", data);
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error fetching user posts:", error);
+      throw error; // Rethrow the error to handle it in the calling code
     });
 };
+
 
 export const addPost = (singlePost) => { 
   return fetch(baseUrl, {
