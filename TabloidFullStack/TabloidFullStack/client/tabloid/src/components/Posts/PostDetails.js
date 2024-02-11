746383e3161140780968f9bdfe22e840ce367f13
JuanAlcalde <jjalcalde001@icloud.com>
2024-02-11 04:59:23 -0500

    fixed buttmon name and details

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Posts/PostDetails.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Posts/PostDetails.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ListGroup, ListGroupItem } from "reactstrap";
 import { getPost } from "../../Managers/PostManager";
 import { useParams } from "react-router-dom";
 import { CurrentPost } from "./CurrentPost";
